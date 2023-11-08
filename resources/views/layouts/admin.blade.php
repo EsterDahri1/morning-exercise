8fbe53a2969952bb4f0dacc326f5f95f4469df88
EsterDahri <esterdahri@gmail.com>
2023-11-08 11:14:03 +0100

    following the teacher

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,115 +1,3 @@
-# Authentication 
-
-<p>For every input exists only one hash. Before saving smth important it is necessary to hash the sensible information.</p>
-
-## Attention! ⚠️
-<p>It is not possible to uncrypt the password. It is not reversible!</p>
-
-## Process
-
-1. Create a new file laravel 
-
-```bash
-
-laravel new name-of-project --git
-
-```
-
-2. Connect the database by modifying the .env file
-
-3. Install Laravel Breeze
-
-```bash
-
-composer require laravel/breeze --dev
-
-```
-4. Install Laravel Breeze
-
-```bash
-
-php artisan breeze:install
-
-```
-5. Select these things in order
-
-<p>blade</p>
-<p>no</p>
-<p>1</p>
-
-6. Install the package for vite ecc.
-
-```bash
-
-composer require pacificdev/laravel_9_preset
-
-```
-
-7. Install bootstrap with --auth 
-
-```bash
-
-php artisan preset:ui bootstrap --auth 
-
-```
-8. Install vite depencencies after modifying vite.config.js in vite.config.cjs or by deleting 'type' = 'modules' in package.json with:
-
-```bash
-
-npm i && npm run dev 
-
-```
-9. Then open the file online with 
-
-```bash
-
-php artisan serve
-
-```
-
-10. Delete edit.blade.php and guest.blade.php because not necessary
-
-11. Open MAMP and make a migration
-```bash
-
-php artisan migrate
-
-```
-<p>then type or select yes</p>
-
-12. Refactor
-
-```bash
-
-php artisan make:controller Admin/DashboardController
-
-```
-
-13. in web.php
-
-```bash
-/*Admin route*/
-Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-});
-
-
-```
-
-14. in DashboardController add function
-
-```bash
-   public function index()
-    {
-        return view('admin.dashboard');
-    }
-```
-15. copy and paste in layouts the app.blade.php and rename it in admin.blade.php and create a new layout 
-
-16. in admin.blade.php add this template and remember to add the dropdown after logout
-
-```admin.blade.php
-
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -123,7 +11,9 @@ Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(fu
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fontawesome 6 cdn -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
+        integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
+        crossorigin='anonymous' referrerpolicy='no-referrer' />
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -138,13 +28,16 @@ Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(fu
 
         <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
             <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolPress</a>
-            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
+                data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
+                aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
             <div class="navbar-nav">
                 <div class="nav-item text-nowrap ms-2">
-                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    <a class="nav-link" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
                         {{ __('Logout') }}
                     </a>
@@ -159,8 +52,8 @@ Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(fu
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a>
-                            <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                            <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
+                            <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
@@ -178,14 +71,15 @@ Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(fu
         <div class="container-fluid vh-100">
             <div class="row h-100">
                 <!-- Definire solo parte del menu di navigazione inizialmente per poi
-								aggiungere i link necessari giorno per giorno
-								 -->
+        aggiungere i link necessari giorno per giorno
+        -->
                 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column">
                             <li class="nav-item">
 
-                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}" href="{{route('admin.dashboard')}}">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.dashboard' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.dashboard') }}">
                                     <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
                                 </a>
                             </li>
@@ -206,12 +100,3 @@ Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(fu
 </body>
 
 </html>
-
-```
-17. In terminal write the command to add storage for future imgs
-
-```bash
-
-php artisan storage:link
-
-```
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/*Admin route*/
+Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
+
+
+
+/*Profile views*/
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
